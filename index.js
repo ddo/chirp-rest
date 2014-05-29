ae1bf97d4610f36a077717b0413d5b4ff3464b35
Ddo <joeddo89@gmail.com>
2014-05-29 18:06:20 +0700

    remove 3 useless lines

--- index.js
+++ index.js
@@ -116,9 +116,6 @@ ChirpRest.prototype.post = function(url, param, callback) {
     */
 
     var body = qs.stringify(self.oauth.authorize(request_data, self.token))
-        .replace(/\-/g, "%2D")
-        .replace(/\_/g, "%5F")
-        .replace(/\./g, "%2E")
         .replace(/\!/g, "%21")
         .replace(/\*/g, "%2A")
         .replace(/\'/g, "%27")
